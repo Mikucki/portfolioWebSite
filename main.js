64b5acfccff78e1f31c594c4b601f654846a435c
Mikucki <piotr.mikucki@op.pl>
2022-07-03 03:17:29 -0400

    added proftolio

---
+++ main.js
@@ -0,0 +1,20 @@
+fotter ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10%;
+    flex-flow: row wrap;
+    font-size: 3rem;
+}
+  
+fotter h2 {
+    text-align: center;
+    margin: 2rem
+}
+  
+fotter p {
+    margin: 2rem;
+    text-align: center;
+    font-size: 0.8rem;
+}
